589dba0d05e0814f864158b071a9acdfe3eaf924
allengw123 <67129544+allengw123@users.noreply.github.com>
2022-08-12 13:19:20 -0400

    EEGlab_Movementstart

--- Functions/Allen Func/EEGlabscripts/runEEGlab.m
+++ Functions/Allen Func/EEGlabscripts/runEEGlab.m
@@ -3,15 +3,15 @@ try
     % Preprocess EEGLab (Epoch, Filter, ICA weights)
     eegevents_icarem=EEGLAB_preprocessing(sbj,protocolfolder,gitpath,save_procPipeline,manual,ica_auto);
 
-    % Time Freq-Analysis (power)
-    eegevents_tfa=EEGLAB_timefreq(eegevents_icarem);
-
-    % FieldTrip
-    eegevents_ft=EEGLAB_imaginarycoh(eegevents_tfa);
-    
-    status='complete';
-    
-    save(fullfile(protocolfolder,sbj,'analysis','EEGlab','EEGlab_Total'),'eegevents_icarem','eegevents_tfa','eegevents_ft','-v7.3');
+%     % Time Freq-Analysis (power)
+%     eegevents_tfa=EEGLAB_timefreq(eegevents_icarem);
+% 
+%     % FieldTrip
+%     eegevents_ft=EEGLAB_imaginarycoh(eegevents_tfa);
+%     
+%     status='complete';
+%     
+%     save(fullfile(protocolfolder,sbj,'analysis','EEGlab','EEGlab_Total'),'eegevents_icarem','eegevents_tfa','eegevents_ft','-v7.3');
 catch ME
     status.message=ME.message;
     status.stack=ME.stack;
